1c967ca6781b255773cb4c86e9ac707130510fc5
余翼 <yuyi@weidian.com>
2017-11-15 11:14:48 +0800

    [chore] index style Support

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const chalk = require('chalk')
 const figures = require('figures')
 
-function symbols(bol, col) {
+function symbols(bol, col, style) {
   let color = col || 'blue'
   let cont = figures[bol] || bol
-  return chalk[color](cont)
+  return style ? chalk[color][style](cont) : chalk[color](cont)
 }
 
 module.exports = symbols
